7d403daf99651b4cecde11d812ba8e46da009852
EC2 Default User <yotchan32323@gmail.com>
2022-12-24 13:33:45 +0000

    chapter13-2

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,8 @@ import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "jquery"
 import "script.js"
+import Swiper from 'swiper/swiper-bundle.js';
+import 'swiper/swiper-bundle.css';
 
 Rails.start()
 Turbolinks.start()
