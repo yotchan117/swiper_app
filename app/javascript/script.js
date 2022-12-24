5f28f6aa38e240e487392ffe501da60f0deeda60
EC2 Default User <yotchan32323@gmail.com>
2022-12-24 13:20:14 +0000

    chapter13-1

---
+++ app/javascript/script.js
@@ -0,0 +1,10 @@
+/* global $*/
+
+let title = "javascriptが使えました";
+alert(title);
+
+$(document).ready(function(){
+  $('.jquery').on('click', function(){
+    $(this).css('color', 'red');
+  });
+});
